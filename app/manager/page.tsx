89c5ee304e2a390cb060c12f30b65aeabf0fbd82
Yliana Faidherbe <ylianafaidherbe@MacBook-Air-de-Yliana.local>
2026-05-29 03:16:53 +0200

    Create manager dashboard

---
+++ app/manager/page.tsx
@@ -0,0 +1,13 @@
+export default function ManagerPage() {
+  return (
+    <main className="p-10 text-white">
+      <h1 className="text-5xl font-bold">
+        Dashboard Manager
+      </h1>
+
+      <p className="mt-4 text-zinc-400">
+        Tableau de bord manager LMG.
+      </p>
+    </main>
+  );
+}
