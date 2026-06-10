2ae4c42c437eec6153532f7e075284402ec9ab7d
Yliana Faidherbe <ylianafaidherbe@MacBook-Air-de-Yliana.local>
2026-06-10 02:31:16 +0200

    Correction photos page team

--- app/site/team/page.tsx
+++ app/site/team/page.tsx
@@ -33,7 +33,7 @@ export default function TeamPage() {
   alt="Joseph"
   width={128}
   height={128}
-  className="h-full w-full scale-150 object-cover object-[center_3%]"
+  className="h-full w-full scale-150 object-cover object-[center_1%]"
 />
             </div>
 
